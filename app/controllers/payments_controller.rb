d634919824c7ce5546e0eb12c1f90d945fe05180
Ankit <ankitkusumakar55954@gmail.com>
2024-11-19 23:45:25 +0530

    Added payment

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,19 @@
+class PaymentsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def scan
+  end
+
+  def ewallet
+  end
+
+  def crypto
+  end
+
+  def bank_transfer
+  end
+end
